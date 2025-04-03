4893877fb2f0106ff7929bf034dc8374988b253a
ZETRON030 <ikechukwulivingspring22@gmail.com>
2025-04-03 02:57:22 +0100

    Add files via upload

---
+++ Ikechukwu_List.py
@@ -0,0 +1,27 @@
+#my_list = []
+#my_list = [10,20,30,40]
+#print(my_list)
+
+#my_list.insert(1,15)
+#print(my_list)
+
+#Using extend method in python
+#my_list = [10,20,30,40]
+#print("List1:",my_list)
+#new_list = [50,60,70]
+#print("List2:",new_list)
+
+#my_list.extend(new_list)
+#print("List after append:",my_list)
+
+#How to remove last element from list
+#my_list.remove(my_list[-1])
+#print(my_list)
+
+#To sort my_list in ascending order
+#my_list.sort()
+#print(my_list)
+
+#Find & print index of value 30 in my_list
+#index_30 = my_list.index(30)
+#print(index_30)
